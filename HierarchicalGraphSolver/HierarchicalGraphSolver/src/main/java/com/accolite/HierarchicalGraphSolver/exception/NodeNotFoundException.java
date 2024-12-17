18f5fbb25216c410abfb0c73c4817390d8ecd4db
Anurag Kumar <anuragkumar07072002@gmail.com>
2024-12-17 15:55:00 +0530

    first commit

---
+++ HierarchicalGraphSolver/HierarchicalGraphSolver/src/main/java/com/accolite/HierarchicalGraphSolver/exception/NodeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.accolite.HierarchicalGraphSolver.exception;
+
+public class NodeNotFoundException extends RuntimeException {
+    public NodeNotFoundException(String message) {
+        super(message);
+    }
+}
+
---
+++ HierarchicalGraphSolver/HierarchicalGraphSolver/src/main/java/com/accolite/HierarchicalGraphSolver/model/Graph.java
@@ -0,0 +1,30 @@
+package com.accolite.HierarchicalGraphSolver.model;
+
+import java.util.*;
+
+public class Graph {
+    private final Map<String, Node> nodes = new HashMap<>();
+    private final Map<String, List<String>> adjacencyList = new HashMap<>();
+
+    public void addNode(Node node) {
+        nodes.put(node.getId(), node);
+        adjacencyList.putIfAbsent(node.getId(), new ArrayList<>());
+    }
+
+    public void addRelationship(String parentId, String childId) {
+        if (nodes.containsKey(parentId) && nodes.containsKey(childId)) {
+            adjacencyList.get(parentId).add(childId);
+            nodes.get(childId).setParentId(parentId);
+        } else {
+            throw new IllegalArgumentException("Parent or child node not found.");
+        }
+    }
+
+    public Map<String, Node> getNodes() {
+        return nodes;
+    }
+
+    public Map<String, List<String>> getAdjacencyList() {
+        return adjacencyList;
+    }
+}
